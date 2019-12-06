54ad6465527fe476be7b60062e250f062453f79f
Ahobert <44192428+Ahobert@users.noreply.github.com>
2019-12-06 15:10:26 +0100

    update matching query to include oa_status
    include esp. oa_status 'closed'

--- ups_wos_matching/matching_bigquery_nov_snapshot.sql
+++ ups_wos_matching/matching_bigquery_nov_snapshot.sql
@@ -1,22 +1,44 @@
 create table oadoi_full.german_unis_oa_categories as (
 select
   w.*,
-  u.is_best as upw_is_best,
-  u.host_type as upw_host_type,
-  u.url as upw_url,
-  u.url_for_pdf as url_pdf,
-  url_for_landing_page as url_land_page
+  u.oa_status,
+  u2.is_best,
+  u2.host_type,
+  u2.url,
+  u2.url_for_pdf,
+  u2.url_for_landing_page
 from
   oadoi_full.doi_list_ger_unis w
 left join (
-  select
-    doi,
-    is_best,
-    host_type,
-    url,
-    url_for_pdf,
-    url_for_landing_page
-  from
-    oadoi_full.mongo_export_upwNov19_08_12, unnest(oa_locations)) u
-  on lower(w.DOI) = u.doi
+    select
+      doi, oa_status
+    from
+      oadoi_full.mongo_export_upwNov19_08_12
+    union all
+    select
+        doi, oa_status
+    from
+      oadoi_full.mongo_export_upwNov19_13_19 ) u
+    on lower(w.DOI) = u.doi
+left join (
+    select
+      doi,
+      is_best,
+      host_type,
+      url,
+      url_for_pdf,
+      url_for_landing_page
+    from
+      oadoi_full.mongo_export_upwNov19_08_12, unnest(oa_locations)
+    union all
+    select
+      doi,
+      is_best,
+      host_type,
+      url,
+      url_for_pdf,
+      url_for_landing_page
+    from
+      oadoi_full.mongo_export_upwNov19_13_19, unnest(oa_locations)) u2
+    on lower(w.DOI) = u2.doi
 )
