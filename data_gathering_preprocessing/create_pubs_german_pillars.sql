9c03593e52767f460971384885dae89fdbb3de9a
Ahobert <anne@hobert.eu>
2021-11-03 09:46:27 +0100

    updated sql query to gather WoS records of german institutions

--- data_gathering_preprocessing/create_pubs_german_pillars.sql
+++ data_gathering_preprocessing/create_pubs_german_pillars.sql
@@ -12,28 +12,28 @@ CREATE TABLE pubs_german_pillars AS
     inst.pk_kb_inst,
     inst.name as inst_name
   FROM
-      wos_b_2019.items items
+      wos_b_2021.items items
   INNER JOIN
-        wos_b_2019.issues issues
+        wos_b_2021.issues issues
             ON issues.pk_issues = items.fk_issues
   INNER JOIN
-        wos_b_2019.databasecollection db
+        wos_b_2021.databasecollection db
             ON db.fk_items =  items.pk_items
    INNER JOIN
-        wos_b_2019.kb_a_addr_inst a_addr_inst
+        wos_b_2021.kb_a_addr_inst a_addr_inst
              ON items.pk_items = a_addr_inst.fk_items
              AND items.ut_eid = a_addr_inst.ut_eid
    INNER JOIN
-        wos_b_2019.kb_inst inst
+        wos_b_2021.kb_inst inst
             ON inst.pk_kb_inst = a_addr_inst.fk_kb_inst
    INNER JOIN
-        wos_b_2019.kb_a_inst_sec a_inst_sec
+        wos_b_2021.kb_a_inst_sec a_inst_sec
             ON inst.pk_kb_inst = a_inst_sec.fk_kb_inst
    INNER JOIN
-        wos_b_2019.kb_sectors sec
+        wos_b_2021.kb_sectors sec
             ON sec.pk_kb_sectors = a_inst_sec.fk_kb_sectors
    INNER JOIN
-        wos_b_2019.kb_a_addr_sec a_addr_sec
+        wos_b_2021.kb_a_addr_sec a_addr_sec
             ON a_addr_sec.fk_kb_a_addr_inst = a_addr_inst.pk_kb_a_addr_inst
             AND a_addr_sec.fk_kb_sectors = sec.pk_kb_sectors
    WHERE
@@ -47,5 +47,5 @@ CREATE TABLE pubs_german_pillars AS
             3, 4, 6, 7, 8, 9, 10
         )
         AND items.pubyear > 2009
-        AND items.pubyear < 2019
+        AND items.pubyear < 2021
 );
